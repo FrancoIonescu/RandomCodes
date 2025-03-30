195a86b240f62d526262f57d05e7c1071e4e63b4
FrancoIonescu <146316338+FrancoIonescu@users.noreply.github.com>
2025-03-30 23:35:45 +0300

    Delete Seminar2GestionareRestaurant directory

--- Seminar2GestionareRestaurant/src/Ingredient.java
+++
@@ -1,9 +0,0 @@
-class Ingredient {
-    String nume;
-    int cantitate;
-
-    public Ingredient(String nume, int cantitate) {
-        this.nume = nume;
-        this.cantitate = cantitate;
-    }
-}
