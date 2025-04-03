b5fddbf7d3b4cefbbcbd8e5bcb8d8f621c83053d
FrancoIonescu <146316338+FrancoIonescu@users.noreply.github.com>
2025-04-03 16:06:52 +0300

    Add files via upload

---
+++ Seminar 6/udp-unit/state.py
@@ -0,0 +1,21 @@
+class State:
+  def __init__(self):
+    self.connections = {}
+  
+  def add_connection(self, address):
+    self.connections.setdefault(address, [])
+  
+  def add_note(self, address, note):
+    self.connections[address].append(note)
+  
+  def get_notes(self, address):
+    return '\n'.join(self.connections[address])
+  
+  def remove_connection(self, address):
+    del self.connections[address]
+  
+  def remove_note(self, address):
+    if address in self.connections and self.connections[address]:
+      self.connections[address].pop()
+      return True
+    return False
